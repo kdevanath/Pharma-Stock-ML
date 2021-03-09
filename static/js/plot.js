e4d2a30c124313f07d32c9dcae1bab05b54dc9ed
gabrielmacey <gabem0928@yahoo.com>
2021-03-09 13:31:44 -0500

    Calling info but need to graph it still

--- static/js/plot.js
+++ static/js/plot.js
@@ -2,57 +2,63 @@ $(function(){
     // names of id go with # symbol (for all)
     // creates an event to pull information
     // based on what we need
-	$('#').click((event) => {
+	$('#dropDown .dropdown-item').click((event) => {
 		$.ajax({
 	    type:"GET",
 	    url: "/facebook_model/<symbol>",
 			data: {
          symbol: event.target.textContent
 	    },
-		});
-		$.ajax({
-			type:"GET",
-			url: "/arima_model/<symbol>",
-			data: {
-				 symbol: event.target.textContent
+			success: function(data) {
+				$('#dropDown .dropdown-menu').hide(200);
+				updateSymbol(data);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				$('#dropDown .dropdown-menu').hide(200);
+				alert(jqXHR.status);
 			},
 		});
 	});
 });
+
 function updateSymbol(facebookData) {
 	let data = {
 	  x: [],
 	  y: [],
 	  type: "line",
 	  marker: {
-	    color: '#1DB954','#441DB9'
+	    color: ['#1DB954','#441DB9']
 	  }
 	};
-	symbolData.forEach((item, i) => {
+	console.log(facebookData);
+	facebookData.forEach((item, i) => {
 		data.x.push(item['date']);
 		data.y.push(item['close']);
 	});
-	console.log(symbolData);
+	console.log(facebookData);
 	const layout = {
 	  title: "<Symbol> Stock Market Values"
 	};
 	Plotly.newPlot("plot", [data], layout);
 
-function updateSymbol(arimaData) {
-	let data = {
-	  x: [],
-	  y: [],
-	  type: "line",
-	  marker: {
-	    color: '#1DB954','#441DB9'
-	  }
 	};
-	symbolData.forEach((item, i) => {
-		data.x.push(item['date']);
-		data.y.push(item['close']);
-	});
-	console.log(symbolData);
-	const layout = {
-	  title: "<Symbol> Stock Market Values"
-	};
-	Plotly.newPlot("plot", [data], layout);
+
+//function updateSymbol(arimaData) {
+	//let data1 = {
+	  //x: [],
+	  //y: [],
+	  //type: "line",
+	  //marker: {
+	    //color: ['#1DB954','#441DB9']
+	  //}
+	//};
+	//symbolData.forEach((item, i) => {
+		//data.x.push(item['date']);
+		//data.y.push(item['close']);
+	//});
+	//console.log(symbolData);
+	//const layout = {
+	  //title: "<Symbol> Stock Market Values"
+	//};
+	//Plotly.newPlot("graph", [data1], layout);
+	//};
