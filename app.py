e9530e33c19693b208ff0b5b8584020dae3f3b6a
gabrielmacey <gabem0928@yahoo.com>
2021-03-08 19:22:12 -0500

    Flask app

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-
-from flask import Flask, jsonify
+from flask import Flask, render_template, redirect, jsonify, request
+import json
 from run_model import *
 
+#Creating an instance
 app = Flask(__name__)
 
+#app.config whatever our connection is
+
 @app.route("/")
-def home():
-    return "I m  here"
+def index():
+    return render_template("HomePage.html")
 
-@app.route("/api/v1.0/model/<symbol>")
-def run_model(symbol):
+@app.route("/facebook_model/<symbol>")
+def run_facebook_model(symbol):
     print(symbol)
-    stock_df = get_stock_data(symbol)
+    #change back to input
+    stock_df = get_stock_data('JNJ')
     return run_facebook_prophet_model(stock_df)
-     
+
+@app.route("/arima_model/<symbol>")
+def run_arima_model(symbol):
+    print(symbol)
+    stock_df2 = get_stock_data(symbol)
+    return run_arima_model(stock_df2)
 
 
-if __name__ == '__main__':
-    app.run(port=5002, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
